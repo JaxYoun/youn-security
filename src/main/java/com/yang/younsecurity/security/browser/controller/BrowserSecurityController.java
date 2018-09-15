4ad9b64b1cca57087f9d9f1092adf2d34e8970e3
JaxYoun <gao88jie@qq.com>
2018-09-15 15:50:55 +0800

    添加不同请求类型返回不同登录提醒注释

--- src/main/java/com/yang/younsecurity/security/browser/controller/BrowserSecurityController.java
+++ src/main/java/com/yang/younsecurity/security/browser/controller/BrowserSecurityController.java
@@ -22,7 +22,9 @@ import java.io.IOException;
 /**
  * @author: Yang
  * @date: 2018/9/11 23:50
- * @description:
+ * @description: 对不同请求类型（主要分为页面型、数据型），如果未认证，此时需要提醒用户认证
+ * 往往需要不同的提醒方式，即页面型就转发的登录页面，但数据型就需要返回401状态码，让前端应用决定，
+ * 这个控制器类就是负责这个事的。
  */
 @Slf4j
 @RestController
@@ -35,14 +37,21 @@ public class BrowserSecurityController {
     @Autowired
     protected CoreSecurityProperties coreSecurityProperties;
 
+    /**
+     * 根据不同请求类型做不同的登录提醒
+     *
+     * @param request
+     * @param response
+     * @return
+     */
     @RequestMapping("/authentication/require")
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
     public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response) {
         SavedRequest savedRequest = requestCache.getRequest(request, response);
-        if(savedRequest != null){
+        if (savedRequest != null) {
             String targetUrl = savedRequest.getRedirectUrl();
             log.info("引发跳转的url是：" + targetUrl);
-            if(StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
+            if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
                 try {
                     this.redirectStrategy.sendRedirect(request, response, this.coreSecurityProperties.getBrowser().getLoginPage());  //登录页面可配置
                 } catch (IOException e) {
