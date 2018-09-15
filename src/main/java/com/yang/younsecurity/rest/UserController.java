9068a2e95574373c01540e53ef4b1a5d625ea8f2
JaxYoun <gao88jie@qq.com>
2018-09-15 17:39:41 +0800

    学习了认证流程-包括那些类，方法，原理，获取principle等测试

--- src/main/java/com/yang/younsecurity/rest/UserController.java
+++ src/main/java/com/yang/younsecurity/rest/UserController.java
@@ -1,6 +1,8 @@
 package com.yang.younsecurity.rest;
 
 import com.yang.younsecurity.domain.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,16 @@ public class UserController {
     @GetMapping("/getUser")
     public Object getUser() {
         return new User(75, "yang");
+    }
+
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("/getCurrentPrincipal")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
     }
 
 }
